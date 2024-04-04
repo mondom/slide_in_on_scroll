0de11d185bea176e8b713e04c0738eab422aaebd
Monika <codingbymon@gmail.com>
2024-04-04 16:36:12 +0200

    creation of 4 variables

--- js.js
+++ js.js
@@ -1,38 +1,42 @@
-
 function debounce(func, wait = 20, immediate = true) {
-    let timeout;
-    return function() {
-        const context = this,
-              args = arguments;
-        
-        const later = () => {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
+	let timeout
+	return function () {
+		const context = this,
+			args = arguments
 
-        const callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
+		const later = () => {
+			timeout = null
+			if (!immediate) func.apply(context, args)
+		}
+
+		const callNow = immediate && !timeout
+		clearTimeout(timeout)
+		timeout = setTimeout(later, wait)
+		if (callNow) func.apply(context, args)
+	}
 }
 
-// Powyższa funkcja to standardowy zapis, który pomaga wywoływać funkcje z określonym przez nas opóźnieniem (szablon z internetu). 
+// Powyższa funkcja to standardowy zapis, który pomaga wywoływać funkcje z określonym przez nas opóźnieniem (szablon z internetu).
 // Dzięki temu mechanizmowi, funkcja przekazana do debounce będzie wywoływana dopiero po ustalonym czasie oczekiwania, a nie przy każdym wywołaniu. Jest to przydatne szczególnie w przypadku funkcji, które mogą być wywoływane często, np. w czasie przewijania strony czy zmiany rozmiaru okna przeglądarki.
 
 const sliderImgs = document.querySelectorAll('.slide-in')
 
-function checkSlide (e) {
-    // console.log(e);
-    // console.count(e)
+function checkSlide(e) {
+	// console.log(e);
+	// console.count(e)
 
-    console.log(window.scrollY);
-    // informacja ile jestśmy od góry okna przeglądarki
-    sliderImgs.forEach(img => {
-        const slideInAt = (window.scrollY + window.innerHeight)
-        // ↑ to nam daje informację ile jesteśmy od dołu okna przeglądarki, bo tego właśnie potrzebujemy do tego projektu
-    })
+	// console.log(window.scrollY);
+	// informacja ile jestśmy od góry okna przeglądarki
+	sliderImgs.forEach(img => {
+		const slideInAt = window.scrollY + window.innerHeight - img.height / 2
+		// ↑ to nam daje informację ile jesteśmy od dołu okna przeglądarki, bo tego właśnie potrzebujemy do tego projektu minus połowa wysokości obrazka
+		console.log(slideInAt)
+        const imageBottom = img.offsetTop + img.height
+        // bottom of the img
 
+        const isHalfShown = slideInAt > img.offsetTop
+        const isNotScrolledPast = window.scrollY < imageBottom
+	})
 }
 
 window.addEventListener('scroll', debounce(checkSlide))
